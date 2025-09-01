801f16bfdfbfa0c42fc71d254c1b421389334a15
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 20:59:59 -0400

    Add Lesson_1_Basic_Selection_from_Series/part_1_pandas_series_example.py

---
+++ Lesson_1_Basic_Selection_from_Series/part_1_pandas_series_example.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Creating a pandas Series
+data = pd.Series([10, 20, 30, 40, 50], index=["a", "b", "c", "d", "e"])
+print(data)
+b    20
+c    30
+d    40
+e    50
+dtype: int64
