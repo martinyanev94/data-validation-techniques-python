c33db6bbcfe4769222da31b075ff638ac23ee3ca
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 21:02:20 -0400

    Add Lesson_4_Overview_of_Pandas_Data_Types/part_2_float_series_example.py

---
+++ Lesson_4_Overview_of_Pandas_Data_Types/part_2_float_series_example.py
@@ -0,0 +1,6 @@
+# Creating a pandas Series with floating-point numbers
+float_series = pd.Series([3.14, 2.718, -0.577], dtype=pd.Float64Dtype())
+print(float_series)
+1    2.718000
+2   -0.577000
+dtype: Float64
