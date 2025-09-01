d6ae820b2bca67689e0b6fc7550e3e9b91b61edd
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 21:00:05 -0400

    Add Lesson_1_Basic_Selection_from_Series/part_5_slice_and_print_data.py

---
+++ Lesson_1_Basic_Selection_from_Series/part_5_slice_and_print_data.py
@@ -0,0 +1,10 @@
+slice_data = data["b":"d"]
+print(slice_data)
+c    30
+d    40
+dtype: int64
+slice_indices = data.iloc[1:4]  # Get elements from index 1 to 3
+print(slice_indices)
+c    30
+d    40
+dtype: int64
