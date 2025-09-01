9557beb469b74bb0e2edf7e601802f66e4c55be0
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 21:01:38 -0400

    Add Lesson_29_Exploratory_Data_Analysis/part_6_scatter_plot_exploration.py

---
+++ Lesson_29_Exploratory_Data_Analysis/part_6_scatter_plot_exploration.py
@@ -0,0 +1,6 @@
+# Create a scatter plot to explore the relationship between two variables
+plt.scatter(df['numerical_column_1'], df['numerical_column_2'], alpha=0.5)
+plt.title('Scatter Plot between Numerical Column 1 and Numerical Column 2')
+plt.xlabel('Numerical Column 1')
+plt.ylabel('Numerical Column 2')
+plt.show()
