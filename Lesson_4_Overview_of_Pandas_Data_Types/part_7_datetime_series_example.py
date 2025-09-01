26578575765de2839ed413ceedb839b9ced0383d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 21:02:27 -0400

    Add Lesson_4_Overview_of_Pandas_Data_Types/part_7_datetime_series_example.py

---
+++ Lesson_4_Overview_of_Pandas_Data_Types/part_7_datetime_series_example.py
@@ -0,0 +1,6 @@
+# Creating a datetime Series
+date_series = pd.Series(["2024-01-01", "2024-01-02", "2024-01-03"], dtype="datetime64[ns]")
+print(date_series)
+1   2024-01-02
+2   2024-01-03
+dtype: datetime64[ns]
