c38fd313d1849e5b01921d65d21ae123c4390297
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 21:01:28 -0400

    Add Lesson_28_foundational_library/part_6_calculate_determinant.py

---
+++ Lesson_28_foundational_library/part_6_calculate_determinant.py
@@ -0,0 +1,8 @@
+from scipy.linalg import det
+
+# Define a square matrix
+matrix = np.array([[1, 2], [3, 4]])
+
+# Calculate the determinant
+determinant = det(matrix)
+print(f'The determinant is: {determinant}')
