d8e87afc150f97d2f9699c5155bd0fc0eb4b1f52
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-31 21:00:07 -0400

    Add Lesson_1_Basic_Selection_from_Series/part_6_check_label_existence.py

---
+++ Lesson_1_Basic_Selection_from_Series/part_6_check_label_existence.py
@@ -0,0 +1,14 @@
+if "z" in data:
+    print("Label 'z' exists in the Series.")
+else:
+    print("Label 'z' does not exist in the Series.")
+
+python
+if "z" in data:
+    print("Label 'z' exists in the Series.")
+else:
+    print("Label 'z' does not exist in the Series.")
+
+
+Label 'z' does not exist in the Series.
+
